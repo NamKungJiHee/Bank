08ca9e77f778acb60f1da17dbed23f3e6e1a6bf6
NamKungJiHee <jihee9711@gmail.com>
2024-07-10 22:35:25 +0900

    add login

--- bank/src/main/java/com/mysite/bank/SecurityConfig.java
+++ bank/src/main/java/com/mysite/bank/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mysite.bank;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,7 +25,14 @@ public class SecurityConfig {
             .headers((headers) -> headers
                 .addHeaderWriter(new XFrameOptionsHeaderWriter(
                     XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-        ;
+            .formLogin((formLogin) -> formLogin
+        		.loginPage("/bank/login")
+        		.defaultSuccessUrl("/"))
+            .logout((logout)-> logout
+        		.logoutRequestMatcher(new AntPathRequestMatcher("/bank/logout"))
+        		.logoutSuccessUrl("/")                                                                                                                                                                                                                                                                                                                                                                                   
+        		.invalidateHttpSession(true));
+        
         return http.build();
     }
 
@@ -31,4 +40,9 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
